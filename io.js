813ad3a8d5162350b474aa2b46e22a6d7223c191
yuval4 <didi19289@gmail.com>
2022-02-05 02:17:14 +0200

    add style to play again button

--- io.js
+++ io.js
@@ -1,5 +1,5 @@
 const socket = io("https://yaniv-server-yuval.herokuapp.com/", {
-  autoConnect: false,
+    autoConnect: false,
 });
 
 // const socket = io("http://127.0.0.1:3000/", {
@@ -7,163 +7,162 @@ const socket = io("https://yaniv-server-yuval.herokuapp.com/", {
 // });
 
 window.onload = () => {
-  const urlParams = new URLSearchParams(window.location.search);
-  history.replaceState({}, null, "/yaniv-client/game.html");
+    const urlParams = new URLSearchParams(window.location.search);
+    history.replaceState({}, null, "/yaniv-client/game.html");
 
-  document.getElementById("my-name").innerHTML = urlParams.get("name");
-  document.getElementById("restart-button").style.display = "none";
-  disableActions(document.getElementById("control-center"), true);
-  disableActions(document.getElementById("my-table"), true);
+    document.getElementById("my-name").innerHTML = urlParams.get("name");
+    document.getElementById("game-over").style.display = "none";
+    disableActions(document.getElementById("control-action"), true);
+    disableActions(document.getElementById("my-table"), true);
 
-  socket.connect();
+    socket.connect();
 
-  socket.emit("onJoinRoom", urlParams.get("code"));
-  socket.emit("onGetPlayerName", urlParams.get("name"));
+    socket.emit("onJoinRoom", urlParams.get("code"));
+    socket.emit("onGetPlayerName", urlParams.get("name"));
 
-  document.getElementById("deck").addEventListener("click", takeCardFromDeck);
+    document.getElementById("deck").addEventListener("click", takeCardFromDeck);
 
-  socket.on("onClientDisconnect", () =>
-    alert("Sorry, the game is over.\nthe other player has left the game.")
-  );
+    socket.on("onClientDisconnect", () =>
+        alert("Sorry, the game is over.\nthe other player has left the game.")
+    );
 
-  socket.on("onRoomFull", (roomName) =>
-    alert(`Room ${roomName} is full, client - disconnected`)
-  );
+    socket.on("onRoomFull", (roomName) =>
+        alert(`Room ${roomName} is full, client - disconnected`)
+    );
 
-  socket.on("onGameStart", (cards) => {
-    const myCards = document.getElementById("my-cards");
-    document.getElementById("restart-button").style.display = "none";
+    socket.on("onGameStart", (cards) => {
+        const myCards = document.getElementById("my-cards");
 
-    // reseting board
-    Array.from(myCards.children).forEach((card) => {
-      card.remove();
-    });
-
-    document.getElementById("winner-banner")?.remove();
-
-    const pile = document.getElementById("pile");
-    Array.from(pile.children).forEach((card) => {
-      card.remove();
-    });
-
-    cards.forEach((card) => {
-      const myCard = createCard(card);
-      myCard.classList.add("my-card");
-      myCard.addEventListener("click", cardPressed);
-      myCards.appendChild(myCard);
-    });
-  });
-
-  socket.on("onPlayersStateChange", (playersState) => {
-    const rivalCards = document.getElementById("rival-cards");
-
-    playersState.forEach((player) => {
-      if (player.id !== socket.id) {
-        document.getElementById("rival-name").innerHTML = player.name;
-        document.getElementById("rival-total-score").textContent =
-          player.totalScore;
-
-        document.querySelectorAll(".rival-card").forEach((card) => {
-          card.remove();
+        // reseting board
+        Array.from(myCards.children).forEach((card) => {
+            card.remove();
         });
 
-        for (let card = 0; card < player.cardsAmount; card++) {
-          const rivalCard = createCard("gray_back");
-          rivalCard.classList.add("rival-card");
-          rivalCards.appendChild(rivalCard);
+        document.getElementById("game-over").style.display = "none";
+
+        const pile = document.getElementById("pile");
+        Array.from(pile.children).forEach((card) => {
+            card.remove();
+        });
+
+        cards.forEach((card) => {
+            const myCard = createCard(card);
+            myCard.classList.add("my-card");
+            myCard.addEventListener("click", cardPressed);
+            myCards.appendChild(myCard);
+        });
+    });
+
+    socket.on("onPlayersStateChange", (playersState) => {
+        const rivalCards = document.getElementById("rival-cards");
+
+        playersState.forEach((player) => {
+            if (player.id !== socket.id) {
+                document.getElementById("rival-name").innerHTML = player.name;
+                document.getElementById("rival-total-score").textContent =
+                    player.totalScore;
+
+                document.querySelectorAll(".rival-card").forEach((card) => {
+                    card.remove();
+                });
+
+                for (let card = 0; card < player.cardsAmount; card++) {
+                    const rivalCard = createCard("gray_back");
+                    rivalCard.classList.add("rival-card");
+                    rivalCards.appendChild(rivalCard);
+                }
+            } else {
+                document.getElementById("my-total-score").textContent =
+                    player.totalScore;
+            }
+        });
+    });
+
+    socket.on("onCardTaken", (hand) => {
+        const myOldCards = document.querySelectorAll(".my-card");
+        myOldCards.forEach((card) => {
+            card.remove();
+        });
+
+        const myCards = document.getElementById("my-cards");
+
+        hand.forEach((card) => {
+            const myCard = createCard(card);
+            myCard.classList.add("my-card");
+            myCard.addEventListener("click", cardPressed);
+            myCards.appendChild(myCard);
+        });
+    });
+
+    socket.on("onPileUpdate", ({ pile: updatedPile, topPile: cardsId }) => {
+        const pile = document.getElementById("pile");
+        const randomRotation = (Math.random() - 0.5) * 90;
+
+        const cardToRemove = Array.from(
+            document.querySelectorAll(".pile-card")
+        ).filter((card) => !updatedPile.includes(card.id));
+
+        if (cardToRemove.length > 0) {
+            cardToRemove[0].remove();
         }
-      } else {
-        document.getElementById("my-total-score").textContent =
-          player.totalScore;
-      }
+
+        removeTopPileCards();
+
+        cardsId.forEach((cardId, index) => {
+            const newCard = createCard(cardId);
+
+            if (index === 0 || index === cardsId.length - 1) {
+                newCard.classList.add("top-pile");
+                newCard.classList.add("pile-card");
+                newCard.addEventListener("click", takeCardFromPile);
+            }
+
+            newCard.style.transform = `rotate(${randomRotation}deg)`;
+            newCard.style.position = "absolute";
+
+            if (cardsId.length > 2) {
+                newCard.style.left = `${80 * (index - 1)}px`;
+                newCard.style.top = `${30 * (index - 1)}px`;
+            } else {
+                newCard.style.left = `${80 * index}px`;
+                newCard.style.top = `${30 * index}px`;
+            }
+
+            const card = document.querySelector(`#my-cards [id='${cardId}']`);
+
+            if (card) {
+                card.remove();
+            }
+
+            pile.appendChild(newCard);
+        });
     });
-  });
 
-  socket.on("onCardTaken", (hand) => {
-    const myOldCards = document.querySelectorAll(".my-card");
-    myOldCards.forEach((card) => {
-      card.remove();
+    socket.on("onTurnChanged", (currentPlayerTrun) => {
+        if (socket.id !== currentPlayerTrun) {
+            disableActions(document.getElementById("control-action"), true);
+            disableActions(document.getElementById("my-table"), true);
+        } else {
+            disableActions(document.getElementById("control-action"), false);
+            disableActions(document.getElementById("my-table"), false);
+        }
     });
 
-    const myCards = document.getElementById("my-cards");
-
-    hand.forEach((card) => {
-      const myCard = createCard(card);
-      myCard.classList.add("my-card");
-      myCard.addEventListener("click", cardPressed);
-      myCards.appendChild(myCard);
+    socket.on("onPlayerWin", ({ winner, state }) => {
+        finishGame(state);
+        document.getElementById("game-over").style.display = "block";
+        const winnerBanner = document.getElementById("winner-banner");
+        winnerBanner.innerText = `the winner is ${winner.name}`;
+        winnerBanner.classList.remove("yaniv-win");
+        winnerBanner.classList.add("yaniv-win");
     });
-  });
 
-  socket.on("onPileUpdate", ({ pile: updatedPile, topPile: cardsId }) => {
-    const pile = document.getElementById("pile");
-    const randomRotation = (Math.random() - 0.5) * 90;
-
-    const cardToRemove = Array.from(
-      document.querySelectorAll(".pile-card")
-    ).filter((card) => !updatedPile.includes(card.id));
-
-    if (cardToRemove.length > 0) {
-      cardToRemove[0].remove();
-    }
-
-    removeTopPileCards();
-
-    cardsId.forEach((cardId, index) => {
-      const newCard = createCard(cardId);
-
-      if (index === 0 || index === cardsId.length - 1) {
-        newCard.classList.add("top-pile");
-        newCard.classList.add("pile-card");
-        newCard.addEventListener("click", takeCardFromPile);
-      }
-
-      newCard.style.transform = `rotate(${randomRotation}deg)`;
-      newCard.style.position = "absolute";
-
-      if (cardsId.length > 2) {
-        newCard.style.left = `${80 * (index - 1)}px`;
-        newCard.style.top = `${30 * (index - 1)}px`;
-      } else {
-        newCard.style.left = `${80 * index}px`;
-        newCard.style.top = `${30 * index}px`;
-      }
-
-      const card = document.querySelector(`#my-cards [id='${cardId}']`);
-
-      if (card) {
-        card.remove();
-      }
-
-      pile.appendChild(newCard);
+    socket.on("onAssaf", ({ winner, state }) => {
+        finishGame(state);
+        document.getElementById("game-over").style.display = "block";
+        const winnerBanner = document.getElementById("winner-banner");
+        winnerBanner.innerText = `assaf! the winner is ${winner.name}`;
+        winnerBanner.classList.remove("yaniv-win");
+        winnerBanner.classList.add("yaniv-win");
     });
-  });
-
-  socket.on("onTurnChanged", (currentPlayerTrun) => {
-    if (socket.id !== currentPlayerTrun) {
-      disableActions(document.getElementById("control-center"), true);
-      disableActions(document.getElementById("my-table"), true);
-    } else {
-      disableActions(document.getElementById("control-center"), false);
-      disableActions(document.getElementById("my-table"), false);
-    }
-  });
-
-  socket.on("onPlayerWin", ({ winner, state }) => {
-    finishGame(state);
-    const winnerBanner = document.createElement("p");
-    document.getElementById("control-center").appendChild(winnerBanner);
-    winnerBanner.innerText = `the winner is ${winner.name}`;
-    winnerBanner.id = "winner-banner";
-    winnerBanner.classList.add("yaniv-win");
-  });
-
-  socket.on("onAssaf", ({ winner, state }) => {
-    finishGame(state);
-    const winnerBanner = document.createElement("p");
-    document.getElementById("control-center").appendChild(winnerBanner);
-    winnerBanner.innerText = `assaf! the winner is ${winner.name}`;
-    winnerBanner.id = "winner-banner";
-    winnerBanner.classList.add("yaniv-win");
-  });
 };
--- yaniv.js
+++ yaniv.js
@@ -1,160 +1,159 @@
 const takeCardFromPile = (event) => {
-  if (document.querySelector(".selected-card")) {
-    const thrownCards = Array.from(
-      document.querySelectorAll(".selected-card")
-    ).map((card) => card.id);
+    if (document.querySelector(".selected-card")) {
+        const thrownCards = Array.from(
+            document.querySelectorAll(".selected-card")
+        ).map((card) => card.id);
 
-    if (isStepValid(thrownCards)) {
-      socket.emit("onCardTakenFromPile", {
-        thrownCards,
-        cardFromPile: event.target.id,
-      });
-    } else {
-      document.querySelectorAll(".selected-card").forEach((card) => {
-        card.classList.add("selected-cards-error");
-        setTimeout(() => {
-          card.classList.remove("selected-cards-error");
-        }, 500);
-      });
+        if (isStepValid(thrownCards)) {
+            socket.emit("onCardTakenFromPile", {
+                thrownCards,
+                cardFromPile: event.target.id,
+            });
+        } else {
+            document.querySelectorAll(".selected-card").forEach((card) => {
+                card.classList.add("selected-cards-error");
+                setTimeout(() => {
+                    card.classList.remove("selected-cards-error");
+                }, 500);
+            });
+        }
     }
-  }
 };
 
 const takeCardFromDeck = () => {
-  if (document.querySelector(".selected-card")) {
-    const thrownCards = Array.from(
-      document.querySelectorAll(".selected-card")
-    ).map((card) => card.id);
+    if (document.querySelector(".selected-card")) {
+        const thrownCards = Array.from(
+            document.querySelectorAll(".selected-card")
+        ).map((card) => card.id);
 
-    if (isStepValid(thrownCards)) {
-      socket.emit("onCardTakeFromDeck", thrownCards);
-    } else {
-      document.querySelectorAll(".selected-card").forEach((card) => {
-        card.classList.add("selected-cards-error");
-        setTimeout(() => {
-          card.classList.remove("selected-cards-error");
-        }, 500);
-      });
+        if (isStepValid(thrownCards)) {
+            socket.emit("onCardTakeFromDeck", thrownCards);
+        } else {
+            document.querySelectorAll(".selected-card").forEach((card) => {
+                card.classList.add("selected-cards-error");
+                setTimeout(() => {
+                    card.classList.remove("selected-cards-error");
+                }, 500);
+            });
+        }
     }
-  }
 };
 
 const createCard = (name) => {
-  const card = document.createElement("img");
+    const card = document.createElement("img");
 
-  card.src = `./assets/cards/${name}.png`;
-  card.classList.add("card");
-  card.setAttribute("id", name);
+    card.src = `./assets/cards/${name}.png`;
+    card.classList.add("card");
+    card.setAttribute("id", name);
 
-  return card;
+    return card;
 };
 
 const cardPressed = (event) => {
-  event.target.classList.toggle("my-card");
-  event.target.classList.toggle("selected-card");
+    event.target.classList.toggle("my-card");
+    event.target.classList.toggle("selected-card");
 };
 
 const removeTopPileCards = () => {
-  document.querySelectorAll(".top-pile").forEach((card) => {
-    card.classList.remove("top-pile");
-    card.classList.remove("top-pile-active");
+    document.querySelectorAll(".top-pile").forEach((card) => {
+        card.classList.remove("top-pile");
+        card.classList.remove("top-pile-active");
 
-    card.removeEventListener("click", takeCardFromPile);
-  });
+        card.removeEventListener("click", takeCardFromPile);
+    });
 };
 
 const calcHandSum = () => {
-  const myHand = Array.from(document.getElementsByClassName("my-card"));
-  return myHand.reduce((total, { id }) => total + cardsToNumber.get(id), 0);
+    const myHand = Array.from(document.getElementsByClassName("my-card"));
+    return myHand.reduce((total, { id }) => total + cardsToNumber.get(id), 0);
 };
 
 const yanivPressed = () => {
-  const yaniv = document.getElementById("yaniv");
+    const yaniv = document.getElementById("yaniv");
 
-  if (calcHandSum() <= 7) {
-    socket.emit("onYaniv", socket.id);
-  } else {
-    yaniv.classList.add("yaniv-error");
+    if (calcHandSum() <= 7) {
+        socket.emit("onYaniv", socket.id);
+    } else {
+        yaniv.classList.add("yaniv-error");
 
-    setTimeout(() => {
-      yaniv.classList.remove("yaniv-error");
-    }, 3000);
-  }
+        setTimeout(() => {
+            yaniv.classList.remove("yaniv-error");
+        }, 3000);
+    }
 };
 
 const isStepValid = (cards) => {
-  if (cards.length === 1) {
-    return true;
-  }
-
-  cards = cards.sort(
-    (currentCard, nexrCard) =>
-      cardsToNumber.get(currentCard) - cardsToNumber.get(nexrCard)
-  );
-
-  let isNumbersFollowing = true;
-
-  for (let index = 0; index < cards.length - 1; index++) {
-    if (
-      cardsToNumber.get(cards[index]) ===
-      cardsToNumber.get(cards[index + 1]) - 1
-    ) {
-      isNumbersFollowing = isNumbersFollowing && true;
-    } else {
-      isNumbersFollowing = isNumbersFollowing && false;
+    if (cards.length === 1) {
+        return true;
     }
-  }
 
-  if (
-    cards.length > 1 &&
-    cards.every(
-      (card) => cardsToNumber.get(card) === cardsToNumber.get(cards[0])
-    )
-  ) {
-    return true;
-  } else if (
-    cards.length > 2 &&
-    isNumbersFollowing &&
-    cards.every((card) => card.slice(-1) === cards[0].slice(-1))
-  ) {
-    return true;
-  }
+    cards = cards.sort(
+        (currentCard, nexrCard) =>
+            cardsToNumber.get(currentCard) - cardsToNumber.get(nexrCard)
+    );
 
-  return false;
+    let isNumbersFollowing = true;
+
+    for (let index = 0; index < cards.length - 1; index++) {
+        if (
+            cardsToNumber.get(cards[index]) ===
+            cardsToNumber.get(cards[index + 1]) - 1
+        ) {
+            isNumbersFollowing = isNumbersFollowing && true;
+        } else {
+            isNumbersFollowing = isNumbersFollowing && false;
+        }
+    }
+
+    if (
+        cards.length > 1 &&
+        cards.every(
+            (card) => cardsToNumber.get(card) === cardsToNumber.get(cards[0])
+        )
+    ) {
+        return true;
+    } else if (
+        cards.length > 2 &&
+        isNumbersFollowing &&
+        cards.every((card) => card.slice(-1) === cards[0].slice(-1))
+    ) {
+        return true;
+    }
+
+    return false;
 };
 
 const finishGame = (playersState) => {
-  const rivalCards = document.getElementById("rival-cards");
+    const rivalCards = document.getElementById("rival-cards");
 
-  playersState.forEach((player) => {
-    if (player.id !== socket.id) {
-      document.getElementById("rival-name").innerHTML = player.name;
+    playersState.forEach((player) => {
+        if (player.id !== socket.id) {
+            document.getElementById("rival-name").innerHTML = player.name;
 
-      document.querySelectorAll(".rival-card").forEach((card) => {
-        card.remove();
-      });
+            document.querySelectorAll(".rival-card").forEach((card) => {
+                card.remove();
+            });
 
-      player.hand.forEach((card) => {
-        const rivalCard = createCard(card);
-        rivalCard.classList.add("rival-card");
-        rivalCards.appendChild(rivalCard);
-      });
-    }
-  });
+            player.hand.forEach((card) => {
+                const rivalCard = createCard(card);
+                rivalCard.classList.add("rival-card");
+                rivalCards.appendChild(rivalCard);
+            });
+        }
+    });
 
-  disableActions(document.getElementById("control-center"), true);
-  disableActions(document.getElementById("my-table"), true);
-  document.getElementById("restart-button").style.display = "block";
+    disableActions(document.getElementById("control-action"), true);
+    disableActions(document.getElementById("my-table"), true);
 };
 
 const restartGame = () => {
-  socket.emit("onRestartGame");
+    socket.emit("onRestartGame");
 };
 
 const disableActions = (element, isAble) => {
-  if (isAble) {
-    element.classList.add("disable");
-  } else {
-    element.classList.remove("disable");
-  }
+    if (isAble) {
+        element.classList.add("disable");
+    } else {
+        element.classList.remove("disable");
+    }
 };
